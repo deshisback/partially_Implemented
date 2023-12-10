eedfdc821443ca1b110e88eb7c33be19a324ac43
Gleb <Aut1Matik@yandex.ru>
2023-12-10 23:15:57 +1000

    Финальная версия проекта

--- least squares/Source.cpp
+++ least squares/Source.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 #include <vector>
 #include <SDL.h>
+#include <SDL_ttf.h>
+#define PI 3.14159265
 
 SDL_Window* win = NULL;
 SDL_Renderer* ren = NULL;
@@ -14,6 +16,7 @@ SDL_Event ev;
 void DeInit(int error) {
 	if (ren != NULL) SDL_DestroyRenderer(ren);
 	if (win != NULL) SDL_DestroyWindow(win);
+	TTF_Quit();
 	SDL_Quit();
 	exit(error);
 }
@@ -35,6 +38,12 @@ void Init() {
 	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 	if (ren == NULL) {
 		std::cout << "Couldn't create renderer! Error:" << SDL_GetError();
+		system("pause");
+		DeInit(1);
+	}
+
+	if (TTF_Init()) {
+		std::cout << "Couldn't init SDL_ttf! Error:" << SDL_GetError();
 		system("pause");
 		DeInit(1);
 	}
@@ -68,34 +77,19 @@ int main(int argc, char* argv[]) {
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
 
-	//int count;
-	//float sums[4] = {0,0,0,0};
-	//float a, b;
-	////std::cin >> count;
-	//count = 10;
-	//std::vector<std::vector<int>> arr(count, std::vector<int>(2));
+	TTF_Font* font = TTF_OpenFont("impact.ttf", 25);
 
-	//SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-	//SDL_RenderClear(ren);
+	char xchar[2] = "x";
+	char ychar[2] = "y";
 
-	//for (int i = 0; i < count; i++) {
-	//	arr[i][0] = rand() % 20;
-	//	arr[i][1] = rand() % 20;
-	//	std::cout << arr[i][0] << " " << arr[i][1] << std::endl;
-	//}
+	SDL_Surface* xsurf = TTF_RenderText_Blended(font, xchar, { 0, 0, 0, 255 });
+	SDL_Surface* ysurf = TTF_RenderText_Blended(font, ychar, { 0,0,0,255 });
 
-	//for (int i = 0; i < count; i++) {
-	//	sums[0] += arr[i][0] * arr[i][1];
-	//	sums[1] += arr[i][0];
-	//	sums[2] += arr[i][1];
-	//	sums[3] += arr[i][0] * arr[i][0];
-	//}
+	SDL_Rect xrect = { 410, 5, xsurf->w, xsurf->h };
+	SDL_Rect yrect = { 780, 410, ysurf->w, ysurf->h };
 
-	//a = ((count * sums[0]) - (sums[1] * sums[2])) / ((sums[3]) - sums[1] * sums[1]);
-	//b = (sums[2] - a * sums[1]) / count;
-
-	//std::cout << "k = " << a << std::endl;
-	//std::cout << "b = " << b << std::endl;
+	SDL_Texture* xtext = SDL_CreateTextureFromSurface(ren, xsurf);
+	SDL_Texture* ytext = SDL_CreateTextureFromSurface(ren, ysurf);
 
 	SDL_SetRenderDrawColor(ren, 187, 187, 187, 255);
 	SDL_RenderClear(ren);
@@ -103,6 +97,7 @@ int main(int argc, char* argv[]) {
 	SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
 
 	double m, v, q, b, alpha;
+	std::cout << "Масса записывается в формате m * 10 ^ -31, скорость в формате v * 10^7, заряд в формате q * 10 ^ -19, значение вектора в формате b * 10^-3" << std::endl;
 	std::cout << "Введите данные частицы: " << std::endl;
 	std::cout << "m = ";
 	std::cin >> m;
@@ -114,13 +109,15 @@ int main(int argc, char* argv[]) {
 	std::cin >> b;
 	std::cout << "angle = ";
 	std::cin >> alpha;
+	
+	std::cout <<std::endl << "m = " << m << " * 10 ^ -31" << std::endl;
+	std::cout << "v = " << v << " * 10 ^ 7" << std::endl;
+	std::cout << "q = " << q << " * 10 ^ -19" << std::endl;
+	std::cout << "B = " << b << " * 10 ^ -3" << std::endl;
 	double x, y;
 	int sx1, sy1, sx2, sy2;
 	double rad = m * v / (q * b);
 	bool isRunning = true;
-	//std::cout << "Масса записывается в формате m * 10 ^ -31, скорость в формате v * 10^7, заряд в формате q * 10 ^ -19, значение вектора в формате b * 10^-3" << std::endl;
-	//std::cout << "Введите массу, скорость, заряд и значение вектора магнитной индукции частицы. ";
-	//std::cin >> m >> v >> q >> b;
 	
 		if ((int)alpha == 90) {
 	
@@ -136,7 +133,6 @@ int main(int argc, char* argv[]) {
 				r.x = sx1 - 1;
 				r.y = sy1 - 1;
 				SDL_RenderFillRect(ren, &r);
-				SDL_RenderPresent(ren);
 			}
 		}
 
@@ -162,50 +158,68 @@ int main(int argc, char* argv[]) {
 
 			for (int i = 0; i <= win_width / 2; i++) {
 				SDL_PollEvent(&ev);
-				y1 = straight(i, t1, t2, s1, s2)/10;
-				y2 = straight(i + 1, t1, t2, s1, s2)/10;
+				y1 = straight(i, t1, t2, s1, s2) / 10;
+				y2 = straight(i + 1, t1, t2, s1, s2) / 10;
 				mathCoordsToScreen(i, y1, 1, win_width / 2, win_heigh / 2, sx1, sy1);
 				mathCoordsToScreen(i + 1, y2, 1, win_width / 2, win_heigh / 2, sx2, sy2);
 
 				SDL_RenderDrawLine(ren, sx1, sy1, sx2, sy2);
-				SDL_RenderPresent(ren);
 			}
 		}
+
+		else {
+
+			double r = (m * v * sin(alpha * PI / 180)) / (abs(q) * b); // считает в радинаха аргумент
+			double h = (2 * PI * m * v * cos(alpha * PI / 180)) / (q * b);
+			r *= 10;
+			h *= 10;
+
+			double temph = 0;
+			int changer = 1;
+			int radius = 0;
+			double dif = r / h;
+
+			for (int i = -win_width / 2; i < win_width / 2; i++) {
+				SDL_PollEvent(&ev);
+				
+				if (temph <= h / 2) {
+
+					mathCoordsToScreen(i, radius * changer * dif, 1, win_width / 2, win_heigh / 2, sx1, sy1);
+					mathCoordsToScreen(i + 1, (radius - 1) * changer * dif, 1, win_width / 2, win_heigh / 2, sx2, sy2);
+					SDL_RenderDrawLine(ren, sx1, sy1, sx2, sy2);
+
+					radius--;
+					temph++;
+				}
+
+				else if (temph > h / 2 && temph < h) {
+
+					mathCoordsToScreen(i, radius * changer * dif, 1, win_width / 2, win_heigh / 2, sx1, sy1);
+					mathCoordsToScreen(i + 1, (radius - 1) * changer * dif, 1, win_width / 2, win_heigh / 2, sx2, sy2);
+					SDL_RenderDrawLine(ren, sx1, sy1, sx2, sy2);
+
+					radius++;
+					temph++;
+				}
+
+				else {
+					temph = 0;
+					changer *= -1;
+					radius = 0;
+				}
+
+			}
+			
+		}
+	
+	SDL_RenderCopy(ren, xtext, NULL, &xrect);
+	SDL_RenderCopy(ren, ytext, NULL, &yrect);
+	SDL_RenderPresent(ren);
 	SDL_Delay(5000);
 
-	///*double x1, y1, x2, y2;
-	//int sx1, sx2, sy1, sy2;
-	//double scale = 2.0;
-	//x1 = -100; x2 = 100;
-	//y1 = 0; y2 = 0;
-	//SDL_Rect r = { 0,0,3,3 };
-	//mathCoordsToScreen(x1, y1, scale, win_width / 2, win_heigh / 2, sx1, sy1);
-	//mathCoordsToScreen(x2, y2, scale, win_width / 2, win_heigh / 2, sx2, sy2);
-	//
-	//SDL_RenderDrawLine(ren, sx1, sy1, sx2, sy2);
-
-	//x1 = 0; x2 = 0;
-	//y1 = -100; y2 = 100;
-	//mathCoordsToScreen(x1, y1, scale, win_width / 2, win_heigh / 2, sx1, sy1);
-	//mathCoordsToScreen(x2, y2, scale, win_width / 2, win_heigh / 2, sx2, sy2);
-
-	//SDL_RenderDrawLine(ren, sx1, sy1, sx2, sy2);
-
-	//for (double alpha = 0; alpha <= 360; alpha += 0.5) {
-	//	x1 = 100 * cos(alpha * M_PI / 180);
-	//	y1 = 100 * sin(alpha * M_PI / 180);
-
-	//	mathCoordsToScreen(x1, y1, scale, win_width / 2, win_heigh / 2, sx1, sy1);
-	//	r.x = sx1 - 1;
-	//	r.y = sy1 - 1;
-	//	SDL_RenderFillRect(ren, &r);
-	//	
-	//}
-
-	//SDL_RenderPresent(ren);
-
-	//SDL_Delay(5000);*/
-
+	SDL_DestroyTexture(ytext);
+	SDL_DestroyTexture(xtext);
+	TTF_CloseFont(font);
 	DeInit(0);
 	return 0;
 }
